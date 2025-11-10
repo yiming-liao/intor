3c2c83181e612ea580497f0f00d08eb73a99f25b
yiming-liao <yimingliao.official@gmail.com>
2025-11-10 18:15:51 +0700

    refactor(hooks): make useTranslateHandlers optional
    Removed the error throw in useTranslateHandlers to allow optional usage of the hook.

--- src/adapters/next/contexts/translate-handlers/hook.ts
+++ src/adapters/next/contexts/translate-handlers/hook.ts
@@ -4,9 +4,5 @@ import { TranslateHandlersContext } from "./context";
 // Hook
 export function useTranslateHandlers() {
   const context = React.useContext(TranslateHandlersContext);
-  if (!context)
-    throw new Error(
-      "useTranslateHandlers must be used within a TranslateHandlersProvider",
-    );
   return context;
 }
