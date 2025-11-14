b1d6f465098a71d308afb02161e493b37332a616
yiming-liao <yimingliao.official@gmail.com>
2025-11-14 09:22:20 +0700

    chore(tests): cleanup unused mocks

--- __test__/mock/mock-fallback-locales.ts
+++
@@ -1,8 +0,0 @@
-import { FallbackLocalesMap } from "intor-translator";
-
-export const mockFallbackLocales: FallbackLocalesMap = {
-  "en-US": ["fr-FR", "zh-TW"],
-  "fr-FR": ["en-US"],
-  "zh-TW": ["en-US"],
-  "ja-JP": [],
-};
