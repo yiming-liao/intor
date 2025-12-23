9fd41d3c2c6a08bdcd2216dbf9b5ccf10ebcb803
yiming-liao <yimingliao.official@gmail.com>
2025-12-23 21:27:14 +0700

    test: update type test script

--- scripts/test-types.ts
+++ scripts/test-types.ts
@@ -15,3 +15,7 @@ run("tsd __test__/types/use-translator/generated");
 // get-translator
 run("tsd __test__/types/get-translator/fallback");
 run("tsd __test__/types/get-translator/generated");
+
+// intor
+run("tsd __test__/types/intor/fallback");
+run("tsd __test__/types/intor/generated");
