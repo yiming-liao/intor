288336a933bedceef08121d2ed610b7e479aea0c
yiming-liao <yimingliao.official@gmail.com>
2026-01-05 19:40:31 +0700

    test: refactor and normalize test structure

--- scripts/test-types.ts
+++ scripts/test-types.ts
@@ -7,15 +7,3 @@ run("tsd __test__/types/config/generated");
 // translator-instance
 run("tsd __test__/types/translator-instance/fallback");
 run("tsd __test__/types/translator-instance/generated");
-
-// use-translator
-run("tsd __test__/types/use-translator/fallback");
-run("tsd __test__/types/use-translator/generated");
-
-// get-translator
-run("tsd __test__/types/get-translator/fallback");
-run("tsd __test__/types/get-translator/generated");
-
-// intor
-run("tsd __test__/types/intor/fallback");
-run("tsd __test__/types/intor/generated");
