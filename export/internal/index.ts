4141d85fbefd044805f21213ef3dcde673d76441
yiming-liao <yimingliao.official@gmail.com>
2026-01-26 12:29:00 +0700

    chore: clean up internal exports and reorder package exports

--- export/internal/index.ts
+++ export/internal/index.ts
@@ -2,11 +2,8 @@
 
 export type {
   // generated
-  INTOR_GENERATED_KEY,
   GenConfigKeys,
   GenConfig,
-  GenMessages,
-  GenLocale,
   // translator-instance
   TranslatorInstance,
 } from "@/core";
